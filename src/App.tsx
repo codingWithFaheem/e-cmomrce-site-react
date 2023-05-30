5f3264ac45143c7fbef5aa8ddc11e217774db5e1
codingWithFaheem <faheemk5914858@gmail.com>
2023-05-30 16:03:21 +0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
+import { Home } from './pages';
+import { NavBar } from './component';
 
 function App() {
   return (
     <div className="app h-screen z-10">
+      <NavBar />
       <Home />
+      
     </div>
   );
 }
 
 export default App;
+  
