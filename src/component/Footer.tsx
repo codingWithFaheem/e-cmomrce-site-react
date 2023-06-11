9accc63b82221fdd26f9199eead1c1336d6a514d
codingWithFaheem <faheemk5914858@gmail.com>
2023-06-11 10:27:04 +0500

    message

---
+++ src/component/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Footer = () => {
+  return (
+    <div className=' h-[20rem] bg-[#123] flex justify-center items-center'>
+            <p className=' md:text-[24px] text-white font-1 tracking-[6px]'>@ copyrigt 2023</p>
+    </div>
+  )
+}
+export default Footer
